0b3b2c022e532303dc1e8a3976c3ab3e01b5b9c1
Valrika Leandre <Mushka@Valrikas-MacBook-Air.local>
2019-02-08 15:57:47 -0500

    Initial commit

---
+++ app/src/main/java/net/androidbootcamp/bookxchange/LoginActivity.java
@@ -0,0 +1,29 @@
+package net.androidbootcamp.bookxchange;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        TextView signUp = (TextView) findViewById(R.id.txtSignUp);
+        Button login = (Button)findViewById(R.id.btnLogin);
+
+        signUp.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
+            }
+        });
+    }
+}

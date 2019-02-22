59be021665b2ef717afea2992258844ab3fc30ef
Mushka <1>
2019-02-22 16:04:07 -0500

    no change

--- app/src/main/java/net/androidbootcamp/bookxchange/SellActivity.java
+++ app/src/main/java/net/androidbootcamp/bookxchange/SellActivity.java
@@ -61,27 +61,4 @@ public class SellActivity extends AppCompatActivity
         }
         return true;
     }
-
-    /*static final int REQUEST_IMAGE_CAPTURE = 1;
-
-    private void dispatchTakePictureIntent()
-    {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null)
-        {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data)
-    {
-        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK)
-        {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            ImageView imgBookPhoto;
-            imgBookPhoto.setImageBitmap(imageBitmap);
-        }
-    }*/
 }

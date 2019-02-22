477a9a3dbeee1c11d94009f19d43b230c6d1ae10
Mushka <1>
2019-02-22 14:47:12 -0500

    added items to activity

--- app/src/main/java/net/androidbootcamp/bookxchange/SellActivity.java
+++ app/src/main/java/net/androidbootcamp/bookxchange/SellActivity.java
@@ -1,11 +1,13 @@
 package net.androidbootcamp.bookxchange;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBar;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 public class SellActivity extends AppCompatActivity
 {
@@ -59,4 +61,27 @@ public class SellActivity extends AppCompatActivity
         }
         return true;
     }
+
+    /*static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent()
+    {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null)
+        {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK)
+        {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ImageView imgBookPhoto;
+            imgBookPhoto.setImageBitmap(imageBitmap);
+        }
+    }*/
 }

27be0c41af43223d37b751d0410c77da917d4b32
Jessica Walker <jwalk161@gmail.com>
2019-02-28 15:33:29 -0500

    fixed login button

--- app/src/main/java/net/androidbootcamp/bookxchange/HelpActivity.java
+++ app/src/main/java/net/androidbootcamp/bookxchange/HelpActivity.java
@@ -1,8 +1,8 @@
 package net.androidbootcamp.bookxchange;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
--- app/src/main/java/net/androidbootcamp/bookxchange/LoginActivity.java
+++ app/src/main/java/net/androidbootcamp/bookxchange/LoginActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,11 +24,14 @@ public class LoginActivity extends AppCompatActivity
     private Button btnRegister, btnLogin;
     private FirebaseAuth auth;
     private ProgressBar progressBar;
+    private String email, password;
+    private static final String TAG = "Login to Account";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
 
         //Get Firebase auth instance
         auth = FirebaseAuth.getInstance();
@@ -37,13 +41,11 @@ public class LoginActivity extends AppCompatActivity
             finish();
         }
 
-        //set view
-        setContentView(R.layout.activity_login);
-
         loginEmail = findViewById(R.id.txtEmailLogin);
         loginPassword = findViewById(R.id.txtLoginPassword);
         btnRegister = findViewById(R.id.btnRegister);
         btnLogin = findViewById(R.id.btnLogin);
+        progressBar = findViewById(R.id.progressBar);
 
         //Get Firebase auth instance
         auth = FirebaseAuth.getInstance();
@@ -60,16 +62,16 @@ public class LoginActivity extends AppCompatActivity
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email = loginEmail.getText().toString();
-                final String password = loginPassword.getText().toString();
+                email = loginEmail.getText().toString();
+                password = loginPassword.getText().toString();
 
                 if (TextUtils.isEmpty(email)) {
-                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "Enter email address", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
                 if (TextUtils.isEmpty(password)) {
-                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "Enter password", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
@@ -86,14 +88,14 @@ public class LoginActivity extends AppCompatActivity
                                 progressBar.setVisibility(View.GONE);
                                 if (!task.isSuccessful()) {
                                     // there was an error
-                                    if (password.length() < 6) {
-                                        loginPassword.setError(getString(R.string.minimum_password));
-                                    } else {
-                                        Toast.makeText(LoginActivity.this, getString(R.string.auth_failed), Toast.LENGTH_LONG).show();
-                                    }
+                                    Log.d(TAG, "auth failed :" + task.getException());
+//                                    if (password.length() < 6) {
+//                                        loginPassword.setError(getString(R.string.minimum_password));
+//                                    } else {
+//                                        Toast.makeText(LoginActivity.this, getString(R.string.auth_failed), Toast.LENGTH_LONG).show();
+//                                    }
                                 } else {
-                                    Intent intent = new Intent(LoginActivity.this, BuyActivity.class);
-                                    startActivity(intent);
+                                    startActivity(new Intent(LoginActivity.this, BuyActivity.class));
                                     finish();
                                 }
                             }

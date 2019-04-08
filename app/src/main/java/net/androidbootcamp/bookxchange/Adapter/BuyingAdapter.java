91b1eefa34dd05214d5b5e6038aa50ec932a299b
Mushka <1>
2019-04-07 21:46:32 -0400

    trying to commit again

---
+++ app/src/main/java/net/androidbootcamp/bookxchange/Adapter/BuyingAdapter.java
@@ -0,0 +1,74 @@
+package net.androidbootcamp.bookxchange.Adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import net.androidbootcamp.bookxchange.model.Book;
+import net.androidbootcamp.bookxchange.R;
+
+import java.util.ArrayList;
+
+public class BuyingAdapter extends RecyclerView.Adapter<BuyingAdapter.ViewHolder>
+{
+    private Context context;
+    private ArrayList<Book> aBook;
+
+    public BuyingAdapter(Context context, ArrayList<Book> aBook)
+    {
+        this.context = context;
+        this.aBook = aBook;
+    }
+//just to commint and send
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+    {
+        View view = LayoutInflater.from(context).inflate(R.layout.card_view, parent, false);
+        return new BuyingAdapter.ViewHolder(view);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position)
+    {
+        final Book book = aBook.get(position);
+        holder.isbn.setText(book.getIsbn());
+        holder.title.setText(book.getTitle());
+        holder.author.setText(book.getAuthor());
+        holder.condition.setText(book.getCondition());
+        holder.price.setText(book.getPrice());
+        holder.user.setText(book.getUid());
+    }
+
+    @Override
+    public int getItemCount()
+    {
+        return aBook.size();
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder
+    {
+        public TextView isbn, title, author, condition, price, user;
+        public ImageView bPic;
+
+        public ViewHolder(@NonNull View itemView)
+        {
+            super(itemView);
+
+            isbn = itemView.findViewById(R.id.txtISBN);
+            title = itemView.findViewById(R.id.txtTitle);
+            author = itemView.findViewById(R.id.txtAuthor);
+            price = itemView.findViewById(R.id.txtPrice);
+            condition = itemView.findViewById(R.id.txtCondition);
+            bPic = itemView.findViewById(R.id.imgBookPhoto);
+            user = itemView.findViewById(R.id.txtUser);
+        }
+    }
+}
---
+++ app/src/main/java/net/androidbootcamp/bookxchange/StartActivity.java
@@ -0,0 +1,61 @@
+package net.androidbootcamp.bookxchange;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class StartActivity extends AppCompatActivity
+{
+    //so i can send it
+
+    Button login;
+    Button register;
+    FirebaseUser firebaseUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        login = findViewById(R.id.btnLogin);
+        register = findViewById(R.id.btnRegister);
+
+        login.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(new Intent(StartActivity.this, LoginActivity.class));
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(new Intent(StartActivity.this, SignUpActivity.class));
+            }
+        });
+    }
+
+    protected void onStart()
+    {
+        super.onStart();
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (firebaseUser != null)
+        {
+            Intent intent = new Intent(StartActivity.this, BuyActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+}
